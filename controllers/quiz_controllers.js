493714ee343fb417412b01d63bb846c21de0afbf
odeib <o.barcos.castera@gmail.com>
2016-05-31 11:39:04 +0200

    Practica 10: Buscar Preguntas

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -18,6 +18,18 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizzes
 exports.index = function(req, res, next) {
+
+if (req.query.search) {
+  var search = req.query.search.split(' ').join('%') ;
+       models.Quiz.findAll({order: 'question', where: {question: {$like : "%"+search+"%" }}})
+   .then(function(quizzes) {
+     res.render('quizzes/index.ejs', { quizzes: quizzes});
+   })
+   .catch(function(error) { 
+  next(error); 
+});
+
+ } else {
 	models.Quiz.findAll()
 		.then(function(quizzes) {
 			res.render('quizzes/index.ejs', { quizzes: quizzes});
@@ -25,6 +37,7 @@ exports.index = function(req, res, next) {
 		.catch(function(error) {
 			next(error);
 		});
+  }
 };
 
 // GET /quizzes/:id
