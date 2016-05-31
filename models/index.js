f14b6a71a57b9293a060fa32524cf5777492d269
odeib <o.barcos.castera@gmail.com>
2016-05-31 19:44:48 +0200

    Tema 19: Tabla de Usuarios

--- models/index.js
+++ models/index.js
@@ -32,6 +32,9 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 // Importar la definicion de la tabla Comments de comment.js
 var Comment = sequelize.import(path.join(__dirname,'comment'));
 
+// Importar la definicion de la tabla Users de user.js
+var User = sequelize.import(path.join(__dirname,'user'));
+
 // Relaciones entre modelos
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
@@ -61,3 +64,4 @@ Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 exports.Comment = Comment; // exportar definición de tabla Comments
+exports.User = User;       // exportar definición de tabla Users
