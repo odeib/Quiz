df469daa37e143a067840826643a9004f0937f12
odeib <o.barcos.castera@gmail.com>
2016-06-01 19:37:24 +0200

    Tema 24: MVC de aceptar comentarios

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,9 @@ router.get('/author', function(req, res, next){
 
  router.param('quizId', quizController.load);  // autoload :quizId
  router.param('userId', userController.load);  // autoload :userId
- 
+ router.param('commentId', commentController.load);  // autoload :commentId
+
+
 // Definición de rutas de sesion
 router.get('/session',    sessionController.new);     // formulario login
 router.post('/session',   sessionController.create);  // crear sesión
@@ -50,7 +52,13 @@ router.get('/quizzes/:quizId(\\d+)/edit',  sessionController.loginRequired, quiz
 router.put('/quizzes/:quizId(\\d+)',       sessionController.loginRequired, quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',    sessionController.loginRequired, quizController.destroy);
 
-router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments',     sessionController.loginRequired, commentController.create);
+ // Definición de rutas de comentarios
+router.get('/quizzes/:quizId(\\d+)/comments/new',  sessionController.loginRequired, 
+	                                               commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments',     sessionController.loginRequired, 
+	                                               commentController.create);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', 
+	                                               sessionController.loginRequired, 
+	                                               commentController.accept);
  
 module.exports = router;
